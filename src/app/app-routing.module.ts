a01c04d423d661e7ee341b71d1ab1bfd4643610c
sobhan <sobhan.ziaee73@gmail.com>
2024-08-09 11:24:08 +0330

    add routing and add prime ng package

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Todolist',
+    loadChildren: () => import('./todolist/todolist.module').then(m => m.TodolistModule)
+  },
+  {
+    path: '',
+    redirectTo: 'Todolist',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'Todolist',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
